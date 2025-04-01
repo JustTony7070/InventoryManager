a94a52ed902a0e9ccc08006dfe915f88bba60a78
Antonio <antoniopin722@gmail.com>
2025-04-01 23:36:28 +0200

    Strumento per scrittura e lettura di file csv

---
+++ InventoryManager/UtilitiesMetods/EasyCsv.cs
@@ -0,0 +1,43 @@
+﻿using CsvHelper;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+
+namespace InventoryManager.UtilitiesMetods
+{
+    static class EasyCsv
+    {
+        public static void Write<T>(string FilePath, List<T> Records)
+        {
+            try
+            {
+                using StreamWriter writer = new(FilePath + ".csv");
+                using CsvWriter csv = new(writer, CultureInfo.InvariantCulture);
+                csv.WriteRecords(Records);
+            }
+            catch
+            {
+                System.Windows.MessageBox.Show("Error While Saving Data", "Saving Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        public static List<T> Load<T>(string Path, bool CreateIfNotExists = true)
+        {
+            try
+            {
+                if (!File.Exists(Path) && CreateIfNotExists)
+                {
+                    File.Create(Path).Close();
+                    return [];
+                }
+                using StreamReader reader = new(Path);
+                using CsvReader csv = new(reader, CultureInfo.InvariantCulture);
+                return [.. csv.GetRecords<T>()];
+            }
+            catch
+            {
+                System.Windows.MessageBox.Show("Error While Loading Data","Loading Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            return [];
+        }
+    }
+}
